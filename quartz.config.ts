5d75dd70f8fca197037b64a0b3f3dbdd05af226a
Fabian-Anna <fabiandirscherl@web.de>
2025-06-02 01:42:11 +0200

    Quartz sync: Jun 2, 2025, 1:42 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,12 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "D&D Travels",
+    pageTitleSuffix: "Reisetagebuch eines Gnoms",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
-    locale: "en-US",
+    analytics: null,
+    locale: "de-DE",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
